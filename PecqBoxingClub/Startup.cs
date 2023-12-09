e753033b56887b1e83fae8bd80f63249f2c4dc8b
Arno Leruste <recroX91@hotmail.com>
2023-12-09 20:21:09 +0100

    Add project files.

---
+++ PecqBoxingClub/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace PecqBoxingClub.Controllers;
+
+public class Startup
+{
+    public Startup(IWebHostEnvironment env, IConfiguration configuration)
+    {
+        Configuration = configuration;
+        Environment = env;
+    }
+
+    public IConfiguration Configuration { get; }
+    public IWebHostEnvironment Environment { get; set; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAnyOriginPolicy",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+        });
+
+        //services.AddGlobalSettingsServices(Configuration);
+
+        //services.AddSwaggerGen(c =>
+        //{
+        //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VotreAPI", Version = "v1" });
+        //});
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        //app.UseSwaggerUI(c =>
+        //{
+        //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "VotreAPI v1");
+        //    c.RoutePrefix = string.Empty;
+        //});
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.UseCors("AllowAnyOriginPolicy");
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
